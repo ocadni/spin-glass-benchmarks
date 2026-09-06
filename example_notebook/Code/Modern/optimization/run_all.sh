087b31e0f0d5b0773f2242243725323ee28f7038
Laplaxe <lucamdelbono@gmail.com>
2026-09-06 10:45:33 +0200

    added .sh dariah scripts

--- example_notebook/Code/Modern/optimization/run_all.sh
+++ example_notebook/Code/Modern/optimization/run_all.sh
@@ -1,5 +1,3 @@
-for repeat in {1..100}; do
-    for n in 50 100; do
-        bash run_greedy_sk.sh $n
-    done
+for repeat in {1..1000}; do
+    bash run_greedy_sk_dariah.sh 50 100 200 300 400 600 800 1000 1200 1400 1600 2000
 done
